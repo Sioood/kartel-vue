5502fba1d2e3f01dc1f4efcdbd9fa8a16006aa15
Sioood <theodupontpro@gmail.com>
2023-02-10 12:20:35 +0100

    💄 style(artwork page): Add a bunch of informations and style it

--- src/pages/ArtworkPage.vue
+++ src/pages/ArtworkPage.vue
@@ -15,9 +15,8 @@ const storeApi = useConfigApi();
 let artwork = ref();
 let authors = ref();
 let authorsName = ref();
-// ref Object Galleries ?
-let processGalleries = ref([]);
-let inSituGalleries = ref([]);
+let galleries = ref({});
+let genres = ref([]);
 
 async function getArtwork(id) {
   let response = await fetch(`${storeApi.restUriV2}production/artwork/${id}`);
@@ -26,17 +25,35 @@ async function getArtwork(id) {
 
   getAuthors(data.authors[0]);
 
-  console.log(data.process_galleries);
+  function getGalleries() {
+    galleries.value.processGalleries = [];
+    data.process_galleries.forEach((el) => {
+      getGallery(el, galleries.value.processGalleries);
+    });
 
-  data.process_galleries.forEach((el) => {
-    getGallery(el, processGalleries.value);
-  });
+    galleries.value.mediationGalleries = [];
+    data.mediation_galleries.forEach((el) => {
+      getGallery(el, galleries.value.mediationGalleries);
+    });
 
-  console.log(data.in_situ_galleries);
+    galleries.value.inSituGalleries = [];
+    data.in_situ_galleries.forEach((el) => {
+      getGallery(el, galleries.value.inSituGalleries);
+    });
 
-  data.in_situ_galleries.forEach((el) => {
-    getGallery(el, inSituGalleries.value);
-  });
+    galleries.value.pressGalleries = [];
+    data.press_galleries.forEach((el) => {
+      getGallery(el, galleries.value.pressGalleries);
+    });
+
+    galleries.value.teaserGalleries = [];
+    data.teaser_galleries.forEach((el) => {
+      getGallery(el, galleries.value.teaserGalleries);
+    });
+  }
+  getGalleries();
+
+  getGenres(data.genres);
 }
 
 // Work for only 1 authors for now
@@ -59,10 +76,7 @@ async function getUser(url) {
   authorsName.value = `${data.first_name} ${data.last_name}`;
 }
 
-// Error If mulitple media we get only the last media, need forEach somewhere maybe
-// or push()
 async function getGallery(url, output) {
-  console.log("getGallery url", url);
   let response = await fetch(url);
   let data = await response.json();
 
@@ -77,7 +91,6 @@ async function getGallery(url, output) {
   });
 
   async function getMedia(url, galleryData, output) {
-    console.log("getMedia", url);
     let response = await fetch(url);
     let data = await response.json();
 
@@ -91,6 +104,20 @@ async function getGallery(url, output) {
   }
 }
 
+async function getGenres(data) {
+  data.forEach((genre) => {
+    async function getGenre(genre) {
+      let response = await fetch(genre);
+      let data = await response.json();
+
+      genres.value.push(data);
+      console.log(genres.value);
+    }
+    getGenre(genre);
+  });
+}
+
+// Need to remove this and all element using this function for Prod
 function removePreprod(url) {
   return url.replace("preprod.", "");
 }
@@ -107,102 +134,205 @@ onMounted(() => {
     v-if="artwork"
     class="pt-2 pr-20 pb-2 w-full min-h-screen flex justify-between gap-10 divide-x"
   >
-    <div class="pl-8 pr-6 py-5 w-3/5 flex flex-col gap-3">
-      <img :src="removePreprod(artwork.picture)" alt="" />
-
-      <UnderlineTitle
-        class="w-max"
-        :title="artwork.title"
-        :underlineSize="1"
-        :fontSize="1"
-      />
-
-      <h3 class="flex items-center text-xl">
-        <h3>{{ artwork.type }} de</h3>
-        <UiLink
-          v-if="authorsName"
-          :text="authorsName"
-          :url="`/artist/${storeApi.getId(authors.url)}`"
+    <div class="pl-8 pr-6 py-5 w-3/5 flex flex-col gap-10">
+      <section class="flex flex-col gap-6">
+        <img :src="removePreprod(artwork.picture)" alt="" />
+        <UnderlineTitle
+          class="w-max"
+          :title="artwork.title"
+          :underlineSize="1"
+          :fontSize="1"
         />
-        —
-        {{ artwork.production_date.split("-")[0] }}
-      </h3>
 
-      <UiDescription
-        :desc_fr="artwork.description_fr"
-        :desc_en="artwork.description_en"
-      />
-      <h3 class="font-bold">Process</h3>
-      {{ processGalleries }}
-      <h3 class="font-bold">In Situ</h3>
-      {{ inSituGalleries }}
+        <h3 class="flex items-center text-xl">
+          <h3>{{ artwork.type }} de</h3>
+          <UiLink
+            v-if="authorsName"
+            :text="authorsName"
+            :url="`/artist/${storeApi.getId(authors.url)}`"
+          />
+          —
+          {{ artwork.production_date.split("-")[0] }}
+        </h3>
+      </section>
 
-      <h3 class="font-bold">Credit fr</h3>
-      <p>{{ artwork.credits_fr }}</p>
-      <p>{{ artwork }}</p>
+      <section class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Description"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+
+        <UiDescription
+          :desc_fr="artwork.description_fr"
+          :desc_en="artwork.description_en"
+        />
+      </section>
+
+      <section class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Crédits"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+
+        <UiDescription
+          :desc_fr="artwork.credits_fr"
+          :desc_en="artwork.credits_en"
+        />
+      </section>
+
+      <section class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Remerciement"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+
+        <UiDescription
+          :desc_fr="artwork.thanks_fr"
+          :desc_en="artwork.thanks_en"
+        />
+      </section>
     </div>
 
     <div
       class="pl-8 pr-6 pt-5 pb-40 sticky top-16 w-2/5 h-screen overflow-x-scroll flex flex-col gap-6"
     >
-      <UnderlineTitle
-        class="w-max"
-        title="Oeuvres"
-        subtitle="Artist"
-        :uppercase="true"
-        :underlineSize="1"
-        :fontSize="2"
-      />
-      <ul class="grid grid-cols-2 gap-3">
-        <!-- Make a function for get galleries and fetch medias -->
-        <li><UiMedia :url="removePreprod(artwork.picture)" title="title" /></li>
-      </ul>
+      <div v-if="genres[0]" class="mb-10 flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Genres"
+          subtitle="Artist"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <div v-for="genre in genres" :key="genre" class="w-full flex flex-col">
+          <div class="p-2 flex gap-1">
+            <h6
+              v-if="genre.label !== '.'"
+              class="px-2 py-1 bg-gray-extralight text-xs font-medium rounded"
+            >
+              {{ genre.label }}
+            </h6>
+          </div>
+          <span class="block w-full h-1 bg-black"></span>
+        </div>
+      </div>
 
-      <UnderlineTitle
-        class="w-max"
-        title="Processus"
-        subtitle="Artist"
-        :uppercase="true"
-        :underlineSize="1"
-        :fontSize="2"
-      />
-      <ul class="grid grid-cols-2 gap-3">
-        <li v-for="gallery in processGalleries" :key="gallery">
-          <UiMedia
-            v-for="media in gallery.mediaData"
-            :key="media"
-            :url="removePreprod(media.picture)"
-            :title="`${media.label} : (${gallery.description})`"
-          />
-          <!-- <UiMedia
-            v-for="media in gallery.mediaData"
-            :key="media"
-            :url="removePreprod(media.picture)"
-            :title="`${media.label} : (${gallery.description})`"
-          /> -->
-        </li>
-      </ul>
+      <div v-if="galleries.teaserGalleries[0]" class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Teaser"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <ul
+          v-for="gallery in galleries.teaserGalleries"
+          :key="gallery"
+          class="grid grid-cols-2 gap-3"
+        >
+          <li v-for="media in gallery.mediaData" :key="media">
+            <UiMedia
+              :url="removePreprod(media.picture)"
+              :title="`${media.label} : (${gallery.description})`"
+            />
+          </li>
+        </ul>
+      </div>
 
-      <UnderlineTitle
-        class="w-max"
-        title="In Situ"
-        subtitle="Artist"
-        :uppercase="true"
-        :underlineSize="1"
-        :fontSize="2"
-      />
-      <ul
-        v-for="gallery in inSituGalleries"
-        :key="gallery"
-        class="grid grid-cols-2 gap-3"
-      >
-        <li v-for="media in gallery.mediaData" :key="media">
-          <UiMedia
-            :url="removePreprod(media.picture)"
-            :title="`${media.label} : (${gallery.description})`"
-          />
-        </li>
-      </ul>
+      <div v-if="galleries.inSituGalleries[0]" class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="In Situ"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <ul
+          v-for="gallery in galleries.inSituGalleries"
+          :key="gallery"
+          class="grid grid-cols-2 gap-3"
+        >
+          <li v-for="media in gallery.mediaData" :key="media">
+            <UiMedia
+              :url="removePreprod(media.picture)"
+              :title="`${media.label} : (${gallery.description})`"
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="galleries.processGalleries[0]" class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Processus"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <ul
+          v-for="gallery in galleries.processGalleries"
+          :key="gallery"
+          class="grid grid-cols-2 gap-3"
+        >
+          <li v-for="media in gallery.mediaData" :key="media">
+            <UiMedia
+              :url="removePreprod(media.picture)"
+              :title="`${media.label} : (${gallery.description})`"
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="galleries.pressGalleries[0]" class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Press"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <ul
+          v-for="gallery in galleries.pressGalleries"
+          :key="gallery"
+          class="grid grid-cols-2 gap-3"
+        >
+          <li v-for="media in gallery.mediaData" :key="media">
+            <UiMedia
+              :url="removePreprod(media.picture)"
+              :title="`${media.label} : (${gallery.description})`"
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="galleries.mediationGalleries[0]" class="flex flex-col gap-6">
+        <UnderlineTitle
+          class="w-max"
+          title="Médiation"
+          :uppercase="true"
+          :underlineSize="1"
+          :fontSize="2"
+        />
+        <ul
+          v-for="gallery in galleries.mediationGalleries"
+          :key="gallery"
+          class="grid grid-cols-2 gap-3"
+        >
+          <li v-for="media in gallery.mediaData" :key="media">
+            <UiMedia
+              :url="removePreprod(media.picture)"
+              :title="`${media.label} : (${gallery.description})`"
+            />
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
