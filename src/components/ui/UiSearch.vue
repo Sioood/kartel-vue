f3a99c5d362e8287615d7a71b10052846ae7f9f5
Sioood <theodupontpro@gmail.com>
2023-02-15 16:53:09 +0100

    💄 style(search): set box-border for add padding to search

--- src/components/ui/UiSearch.vue
+++ src/components/ui/UiSearch.vue
@@ -3,13 +3,13 @@
 <template>
   <label
     for="search"
-    class="w-max flex flex-col items-end text-xs text-gray-500"
+    class="p-1 w-max flex flex-col items-end text-xs text-gray-500"
   >
     <input
       type="search"
       name="search"
       id="search"
-      class="w-full text-lg text-black font-bold capitalize focus:bg-gray-100 transition-all duration-500"
+      class="px-2 py-1 box-border w-full text-lg text-black font-bold capitalize focus:bg-gray-100 transition-all duration-500"
     />
     <span class="w-full h-1 bg-black dark:bg-white"></span>
     SEARCH
