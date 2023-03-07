ac9dc51b117ef3734a6ba42c412491e9a20d6450
Sioood <theodupontpro@gmail.com>
2023-03-07 12:08:06 +0100

    ➖➕ build(cypress): add and remove dependency for unit test
    ♻️ refactor mount and exposed instances to acess it. Not very confident with the method...

--- src/components/__tests__/UnitExample.cy.js
+++ src/components/__tests__/UnitExample.cy.js
@@ -1,5 +1,6 @@
 // import files and functions which are exported in components/stores...
 import { shallowMount } from "@vue/test-utils";
+import { mount } from "@cypress/vue";
 import "@/main.js";
 
 import { useConfigApi } from "@/stores/configApi";
@@ -22,18 +23,60 @@ describe("function", () => {
   });
 
   it("Get a method function and a variable of the component and test it", () => {
+    /**
+    
+      You need to expose variables & methods inside a vue "script setup" component by defineExpose({ variable or method });
+    
+    **/
+
+    /**
+    
+      VUE TEST UTILS
+    
+    **/
+
     // this help me a lot 👉 https://stackoverflow.com/questions/71342022/jest-unit-test-cannot-find-component-functions-when-using-vue3-script-setup-synt
-    // with vue test utils
-    // and shallowMount
+    // with vue test utils and shallowMount
 
-    const wrapper = shallowMount(UnitExample);
+    const wrapperTestUtils = shallowMount(UnitExample, {
+      props: { title: "Mount from vue test utils" },
+    });
 
-    expect(wrapper.exists());
-    // Need to defineExpose({ test }); inside the component if it have "script setup"
-    expect(wrapper.vm.test()).equal("test");
+    expect(wrapperTestUtils.exists());
+    expect(wrapperTestUtils.vm.test()).equal("exposed test");
+    expect(wrapperTestUtils.vm.returnString("string")).equal("string");
+    expect(wrapperTestUtils.vm.variable).equal("i'm a variable");
 
-    expect(wrapper.vm.variable).equal("i'm a variable");
+    /**
+    
+      CYPRESS/VUE
+    
+    **/
 
-    cy.mount(UnitExample);
+    //                                             👇
+    // cypress docs have a missing parenthese "then'('({ wrapper, component }) =>..."
+    // cy.mount(COMPONENT).then(({ wrapper, component }) => {
+    //   // `wrapper` is the Vue Test Utils wrapper
+    //   // `component` is the component instance itself
+    // });
+
+    const wrapperCypress = mount(
+      UnitExample,
+      {
+        props: {
+          title:
+            "Mount from cypress/vue because test utils don't run in browser",
+        },
+      },
+      { shallow: true }
+    );
+
+    // i don't know why we can't access to vm directly but vm._.exposed can access to the exposed instances.
+    wrapperCypress.then(({ wrapper }) => {
+      expect(wrapper.exists());
+      expect(wrapper.vm._.exposed.test()).equal("exposed test");
+      expect(wrapper.vm._.exposed.returnString("string")).equal("string");
+      expect(wrapper.vm._.exposed.variable).equal("i'm a variable");
+    });
   });
 });
--- src/components/__tests__/UnitExample.vue
+++ src/components/__tests__/UnitExample.vue
@@ -1,21 +1,29 @@
 <script setup>
 import { sum, value } from "./UnitExample.js";
 
+defineProps(["title"]);
+
 let variable = "i'm a variable";
 
 function test() {
-  return "test";
+  return "exposed test";
+}
+
+function returnString(string) {
+  return string;
 }
 
 // defineExpose allows methods and variable to be accessed out of the component. <script setup> is by default closed
-defineExpose({ test, variable });
+defineExpose({ test, variable, returnString });
 </script>
 
 <template>
-  <div>
+  <div class="p-4">
+    <h1 class="text-xl font-medium underline">{{ title }}</h1>
     <h2>sum(): {{ sum(1, 1) }}</h2>
     <h2>value: {{ value }}</h2>
     <h2>variable: {{ variable }}</h2>
+    <h2>test: {{ test() }}</h2>
   </div>
 </template>
 
