6279f218e05aa171ecb4dd373f33681d054da2b4
Sioood <theodupontpro@gmail.com>
2023-03-20 13:06:01 +0100

    ✅ test: add test for getContent
    coverage: branch result might be better

---
+++ src/__tests__/getContent.test.js
@@ -0,0 +1,84 @@
+import config from "@/config";
+
+import { flushPromises } from "@vue/test-utils";
+
+import { getContent, content, offset, load } from "@/composables/getContent";
+
+import artworkFixture from "~/fixtures/artwork.json";
+import artistFixture from "~/fixtures/artist.json";
+
+// function to mock a response to a promise response
+function createMockResolveValue(data) {
+  return {
+    json: () => new Promise((resolve) => resolve(data)),
+    ok: true,
+  };
+}
+
+let params = {
+  genres: null,
+  keywords: null,
+  productionYear: null,
+  q: null,
+  shootingPlace: null,
+  type: null,
+};
+
+describe("test the composable getContent", () => {
+  const mockFetch = vi.spyOn(global, "fetch");
+
+  mockFetch
+    .mockReturnValue(
+      // default mock but not the first
+      createMockResolveValue({
+        default: true,
+      })
+    )
+    // if once is present it would be the first mock and switch to the next mock or return to the default mock if no next
+    .mockReturnValueOnce(createMockResolveValue(artworkFixture))
+    .mockReturnValueOnce(createMockResolveValue([artistFixture]));
+
+  afterEach(() => {
+    mockFetch.mockClear();
+
+    // reset default value
+    content.value = [];
+    offset.value = 1;
+    load.value = false;
+  });
+
+  it("check content for artwork", async () => {
+    // check default value
+    expect(content.value).toEqual([]);
+    expect(load.value).toEqual(false);
+    expect(offset.value).toEqual(1);
+
+    // console.log(artist);
+    await getContent("artwork", params);
+    
+    // leave the requests and replace with mocks
+    await flushPromises();
+
+    // check value after running once getContent
+    expect(content.value).toEqual(artworkFixture);
+    expect(load.value).toEqual(true);
+    expect(offset.value).toEqual(2);
+  });
+
+  it("check content for artist", async () => {
+    // check default value
+    expect(content.value).toEqual([]);
+    expect(load.value).toEqual(false);
+    expect(offset.value).toEqual(1);
+
+    await getContent("artist", params);
+
+    // leave the requests and replace with mocks
+    await flushPromises();
+
+    // check value after running once getContent
+    expect(content.value).toEqual([artistFixture]);
+    expect(load.value).toEqual(true);
+    expect(offset.value).toEqual(2);
+  });
+});
