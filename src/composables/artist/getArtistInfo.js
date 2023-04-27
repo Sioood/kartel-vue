398b67cfb26be46ef3a1e2f9470be059ca3f4ecd
Sioood <theodupontpro@gmail.com>
2023-04-27 11:27:37 +0200

    ♻️ refactor: update artist composable and a watcher route updater for artist and artwork pages
    - update some style

--- src/composables/artist/getArtistInfo.js
+++ src/composables/artist/getArtistInfo.js
@@ -4,7 +4,7 @@
 
 import axios from "axios";
 
-import { ref, onBeforeMount } from "vue";
+import { ref } from "vue";
 
 import { getId } from "@/composables/getId";
 
@@ -15,210 +15,212 @@ import { getId } from "@/composables/getId";
  * @param {string} auth - the token of the user
  *
  */
-export const getArtistInfo = (artistId, auth) => {
-  /**
-   * @type {object} artist
-   * @type {array} artwork
-   * @type {object} student
-   * @type {object} user
-   * @type {object} candidature
-   */
-  let artist = ref();
-  let artwork = ref();
-  let student = ref();
-  let user = ref();
-  let candidature = ref();
+// export const getArtistInfo = (artistId, auth) => {
+/**
+ * @type {object} artist
+ * @type {array} artwork
+ * @type {object} student
+ * @type {object} user
+ * @type {object} candidature
+ */
+let artist = ref();
+let artwork = ref();
+let student = ref();
+let user = ref();
+let candidature = ref();
 
-  /**
-   *  set token and get user information with the id url of artist.user
-   *  If the token is empty it's means that the user is not authenticated and set empty string.
-   *
-   * @type {string} token
-   */
-  let token = localStorage.getItem("token") || "";
+/**
+ *  set token and get user information with the id url of artist.user
+ *  If the token is empty it's means that the user is not authenticated and set empty string.
+ *
+ * @type {string} token
+ */
+let token = localStorage.getItem("token") || "";
 
-  /**
-   *  function for fetching artist data
-   *
-   * @param {string} id (number id) - the id of the artist
-   *
-   */
-  async function getArtist(id) {
-    try {
-      const response = await axios.get(`people/artist/${id}`);
+/**
+ *  function for fetching artist data
+ *
+ * @param {string} id (number id) - the id of the artist
+ *
+ */
+async function getArtist(id) {
+  try {
+    const response = await axios.get(`people/artist/${id}`);
 
-      const data = response.data;
+    const data = response.data;
 
-      artist.value = data;
-    } catch (err) {
-      console.error(err);
-      artist.value = {};
-    }
-
-    // get user information with the id url of artist.user
-    // getUser(getId(data.user));
+    artist.value = data;
+  } catch (err) {
+    console.error(err);
+    artist.value = {};
   }
 
-  /**
-   *  function for fetching user data of artist
-   *
-   * @param {string} id (number id) - the id of the artist
-   *
-   */
-  async function getUser(id) {
-    let headers = {
-      "Content-Type": "application/json;charset=UTF-8",
-    };
+  // get user information with the id url of artist.user
+  // getUser(getId(data.user));
+}
 
-    if (token) {
-      console.log(true);
-      headers.Authorization = `JWT ${token}`;
-    }
+/**
+ *  function for fetching user data of artist
+ *
+ * @param {string} id (number id) - the id of the artist
+ *
+ */
+async function getUser(id) {
+  let headers = {
+    "Content-Type": "application/json;charset=UTF-8",
+  };
 
-    try {
-      const response = await axios.get(`people/user/${id}`, {
-        headers,
-      });
-
-      const data = response.data;
-
-      user.value = data;
-
-      // prevent if the user have a token but it's not a good token fetch without it
-      // if (response.status === 401) {
-      //   try {
-      //     const response = await axios.get(`people/user/${id}`, {
-      //       headers: {
-      //         "Content-Type": "application/json;charset=UTF-8",
-      //       },
-      //     });
-
-      //     const data = response.data;
-
-      //     user.value = data;
-      //   } catch (err) {
-      //     console.error(err);
-      //     user.value = {};
-      //   }
-      // } else {
-      //   user.value = data;
-      // }
-    } catch (err) {
-      console.error(err);
-      user.value = {};
-    }
+  if (token) {
+    console.log(true);
+    headers.Authorization = `JWT ${token}`;
   }
 
-  /**
-   *  Fetch candidature from username
-   *
-   * @param {string} username - the username of the artist
-   */
-  async function getCandidature(username) {
-    try {
-      const response = await axios.get(
-        `school/student-application?search=${username}`,
-        {
-          headers: {
-            "Content-Type": "application/json;charset=UTF-8",
-            // set the token everytime, if the user is not authenticated it's empty and the api send only "not authenticated" informations
-            Authorization: `JWT ${token}`,
-          },
-        }
-      );
+  try {
+    const response = await axios.get(`people/user/${id}`, {
+      headers,
+    });
 
-      const data = response.data;
+    const data = response.data;
 
-      if (data.length > 0) {
-        // get the candidature in data which have selected true
-        candidature.value = data.filter((item) => item.selected)[0];
+    user.value = data;
+
+    // prevent if the user have a token but it's not a good token fetch without it
+    // if (response.status === 401) {
+    //   try {
+    //     const response = await axios.get(`people/user/${id}`, {
+    //       headers: {
+    //         "Content-Type": "application/json;charset=UTF-8",
+    //       },
+    //     });
+
+    //     const data = response.data;
+
+    //     user.value = data;
+    //   } catch (err) {
+    //     console.error(err);
+    //     user.value = {};
+    //   }
+    // } else {
+    //   user.value = data;
+    // }
+  } catch (err) {
+    console.error(err);
+    user.value = {};
+  }
+}
+
+/**
+ *  Fetch candidature from username
+ *
+ * @param {string} username - the username of the artist
+ */
+async function getCandidature(username) {
+  try {
+    const response = await axios.get(
+      `school/student-application?search=${username}`,
+      {
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          // set the token everytime, if the user is not authenticated it's empty and the api send only "not authenticated" informations
+          Authorization: `JWT ${token}`,
+        },
       }
-    } catch (err) {
-      console.error(err);
+    );
+
+    const data = response.data;
+
+    if (data.length > 0) {
+      // get the candidature in data which have selected true
+      candidature.value = data.filter((item) => item.selected)[0];
     }
+  } catch (err) {
+    console.error(err);
   }
+}
 
-  /**
-   *  Fetch artwork data form the artist
-   *
-   * @param {string} id (number id) - the id of the artist
-   *
-   */
-  async function getArtworks(id) {
-    try {
-      const response = await axios.get(`production/artwork?authors=${id}`);
+/**
+ *  Fetch artwork data form the artist
+ *
+ * @param {string} id (number id) - the id of the artist
+ *
+ */
+async function getArtworks(id) {
+  try {
+    const response = await axios.get(`production/artwork?authors=${id}`);
 
-      const data = response.data;
+    const data = response.data;
 
-      artwork.value = data;
-    } catch (err) {
-      console.error(err);
-      artwork.value = {};
-    }
+    artwork.value = data;
+  } catch (err) {
+    console.error(err);
+    artwork.value = {};
   }
+}
 
-  /**
-   *  Fetch student data of the artist
-   *
-   * @param {string} id (number id) - the id of the artist
-   *
-   */
-  async function getStudent(id) {
+/**
+ *  Fetch student data of the artist
+ *
+ * @param {string} id (number id) - the id of the artist
+ *
+ */
+async function getStudent(id) {
+  try {
+    const response = await axios.get(`school/student?artist=${id}`);
+
+    const studentData = response.data;
+
     try {
-      const response = await axios.get(`school/student?artist=${id}`);
+      const response = await axios.get(studentData[0].promotion);
 
-      const studentData = response.data;
+      let promotionData = response.data;
 
-      try {
-        const response = await axios.get(studentData[0].promotion);
+      studentData[0].promotion = promotionData;
 
-        let promotionData = response.data;
-
-        studentData[0].promotion = promotionData;
-
-        student.value = studentData;
-      } catch (err) {
-        student.value = {};
-      }
-
-      // student.value = data;
+      student.value = studentData;
     } catch (err) {
       student.value = {};
     }
+
+    // student.value = data;
+  } catch (err) {
+    student.value = {};
   }
+}
 
-  onBeforeMount(async () => {
-    // await the artist data for get the user url to not exec the function getUser inside
-    await getArtist(artistId);
+async function setup(artistId, auth) {
+  console.log(artistId);
+  // await the artist data for get the user url to not exec the function getUser inside
+  await getArtist(artistId);
 
-    await getUser(getId(artist.value.user));
+  await getUser(getId(artist.value.user));
 
-    getArtworks(artistId);
+  getArtworks(artistId);
 
-    getStudent(artistId);
+  getStudent(artistId);
 
-    // auth is checked by invert because if checked by a !local.storage which invert boolean response
-    if (!auth) {
-      console.log(auth);
-      getCandidature(user.value.username);
-    }
-  });
+  // auth is checked by invert because if checked by a !local.storage which invert boolean response
+  if (!auth) {
+    console.log(auth);
+    getCandidature(user.value.username);
+  }
+}
 
-  /**
-   *  @exports data for access outside
-   */
-  return {
-    // define access to the ref needed inside the template
-    artist,
-    user,
-    artwork,
-    student,
-    candidature,
+/**
+ *  @exports data for access outside
+ */
+export {
+  setup,
+  // define access to the ref needed inside the template
+  artist,
+  user,
+  artwork,
+  student,
+  candidature,
 
-    // export functions for test
-    getArtist,
-    getUser,
-    getArtworks,
-    getStudent,
-  };
+  // export functions for test
+  getArtist,
+  getUser,
+  getArtworks,
+  getStudent,
 };
+// };
