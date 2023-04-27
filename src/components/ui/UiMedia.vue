2d9ff4fbaa12f0bab3d3a323a18c3ff0b02a8a3f
Sioood <theodupontpro@gmail.com>
2023-04-27 12:28:27 +0200

    ✨ feat: introduce images gallery with fancybox

--- src/components/ui/UiMedia.vue
+++ src/components/ui/UiMedia.vue
@@ -1,8 +1,6 @@
 <script setup>
 // import config from "@/config";
-import config from "../../config";
-
-import { ref } from "vue";
+import config from "@/config";
 
 const props = defineProps({
   url: {
@@ -16,73 +14,34 @@ const props = defineProps({
     required: false,
   },
 });
-
-const open = ref(false);
 </script>
 
 <!-- Change the name with MediaCard might be better for reuse -->
 <!-- set for img if preview but it can be a video watch out -->
 <template>
   <div class="flex flex-col items-end">
-    <!-- Lightbox -> can be a library -->
-    <Teleport v-if="open" to="body">
-      <div
-        @click="open = false"
-        class="z-40 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-black/[.5]"
-      ></div>
-      <div
-        class="z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95vw] lg:w-2/3 max-h-[90svh] flex flex-col items-center justify-center overflow-y-scroll"
-      >
-        <span class="block w-full h-1 bg-black"></span>
-        <div
-          class="h-full max-w-max flex flex-col items-center justify-center bg-gray-extralightest"
-        >
-          <div class="px-2 py-2 w-full flex items-center justify-between gap-3">
-            <h2 class="w-3/4 text-2xl font-medium">
-              {{ props.title }}
-            </h2>
-            <button
-              @click="open = false"
-              class="px-4 py-2 bg-black text-white text-sm"
-            >
-              Close
-            </button>
-          </div>
-          <!-- Can be a video, need to make a condition and check for a preview -->
-          <img
-            v-if="!props.medium"
-            class="w-full h-full object-cover aspect-video"
-            :src="
-              props.url
-                ? `${config.media_service}?url=${props.url}&mode=adapt&w=1000&fmt=jpg`
-                : ''
-            "
-            :alt="props.title"
-          />
-          <video
-            v-else
-            class="w-full h-full object-cover aspect-video"
-            controls
-            autoplay
-            :src="props.medium"
-          ></video>
-        </div>
-      </div>
-    </Teleport>
-
     <div
       @click="open = true"
       class="w-full aspect-video object-cover bg-gray-extralightest cursor-pointer"
     >
-      <img
-        class="w-full h-full object-cover aspect-video"
-        :src="
-          props.url
-            ? `${config.media_service}?url=${props.url}&mode=adapt&w=300&fmt=jpg`
-            : ''
+      <a
+        data-fancybox="gallery"
+        :href="
+          props.medium
+            ? props.medium
+            : `${config.media_service}?url=${props.url}&mode=adapt&w=1000&fmt=jpg`
         "
-        :alt="props.title"
-      />
+      >
+        <img
+          class="w-full h-full object-cover aspect-video"
+          :src="
+            props.url
+              ? `${config.media_service}?url=${props.url}&mode=adapt&w=300&fmt=jpg`
+              : ''
+          "
+          :alt="props.title"
+        />
+      </a>
     </div>
     <span class="block w-full h-1 bg-black"></span>
     <h6 class="text-xs text-gray font-medium uppercase">{{ props.title }}</h6>
